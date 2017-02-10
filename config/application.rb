f60d491743fc92650aa5c697cd5fd000069806f0
Eric-Guo <guochunzhong@bayekeji.com>
2017-02-10 16:38:43 +0800

    Timezone now in local

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,8 @@ module LandlordRapi
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Beijing'
+    config.active_record.default_timezone = :local
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
