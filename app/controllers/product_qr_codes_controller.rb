ce294217156d61c7590ee22b8e48955f8d20a77d
although2013 <althoughghgh@gmail.com>
2017-02-16 10:49:41 +0800

    购物车二维码生成

--- app/controllers/product_qr_codes_controller.rb
+++ app/controllers/product_qr_codes_controller.rb
@@ -1,7 +1,11 @@
 class ProductQrCodesController < BaseController
   VERSION = 1
 
+
+  # WARN: deprecate method!
   def image
+    return render status: 403
+
     file_name = check_page_path
     return render status: 403, json: { errcode: 1, msg: '这个小程序地址不在rapi白名单中' } if file_name.nil?
 
@@ -12,6 +16,23 @@ class ProductQrCodesController < BaseController
     end
 
     url = request.base_url + '/page_qr_codes/' + file_name
+    render json: { url: url }
+  end
+
+  def render_pay_qr_image
+    key = 'pay_' + SecureRandom.hex(16)
+    qr_image_url = "pages/cart/cart?ticket=#{key}"
+    temp_info = params.to_json
+
+    $redis.set(key, temp_info)
+    $redis.expire(key, 7200)
+
+    file_base_name = "#{key}.jpg"
+    file_path = Rails.root.join('public', 'page_qr_codes', file_base_name)
+    file = Wechat.api.wxa_create_qrcode(qr_image_url).open
+    IO.binwrite(file_path, file.read)
+
+    url = request.base_url + '/page_qr_codes/' + file_base_name
     render json: { url: url }
   end
 
