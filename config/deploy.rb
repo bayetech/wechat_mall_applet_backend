09909bb16a5cde15fac0ee751d1c2a8ceef86275
Eric-Guo <guochunzhong@bayekeji.com>
2017-03-11 10:13:31 +0800

    Upgrade Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '~> 3.7.0'
+lock '~> 3.8.0'
 
 set :application, 'landlord_rapi'
 set :repo_url, 'git@source.bayekeji.com:backend/landlord_rapi.git'
