52735e792b39cb80d9d4c9fad07c33d9bc4b2615
Eric-Guo <guochunzhong@bayekeji.com>
2017-07-02 17:06:30 +0800

    MIssing one place of rename landlord_rapi to rapi.

--- config/puma-web.rb
+++ config/puma-web.rb
@@ -1,4 +1,4 @@
-app_root = '/var/www/landlord_rapi/current'
+app_root = '/var/www/rapi/current'
 pidfile "#{app_root}/tmp/pids/puma.pid"
 state_path "#{app_root}/tmp/pids/puma.state"
 stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
