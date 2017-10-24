06537c8ad8599fa862ceb59f97dea9154dc81144
Eric-Guo <guochunzhong@bayekeji.com>
2017-10-24 21:46:14 +0800

    upgrade gems

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '~> 3.9.0'
+lock '~> 3.10.0'
 
 set :application, 'rapi'
 set :repo_url, 'git@github.com:bayetech/wechat_mall_applet_backend.git'
